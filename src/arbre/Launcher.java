5cc9e4802d245fea2b888df65f5107c21e53d86e
SyberLeCyber <decultot.louis@gmail.com>
2018-06-01 16:26:28 +0200

    Release 1.1.0

---
+++ src/arbre/Launcher.java
@@ -0,0 +1,82 @@
+package arbre;
+
+import java.util.Scanner;
+
+public class Launcher {
+
+	static Scanner sc = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		//Node<Integer> nodes = initialize();
+		
+		Node<Integer> nodes = new Node<Integer>(2);
+//        Node<Integer> n11 = new Node<Integer>(7);
+//        Node<Integer> n12 = new Node<Integer>(5);
+//        Node<Integer> n21 = new Node<Integer>(2);
+//        Node<Integer> n22 = new Node<Integer>(6);
+//        Node<Integer> n23 = new Node<Integer>(9);
+//        Node<Integer> n31 = new Node<Integer>(5);
+//        Node<Integer> n32 = new Node<Integer>(8);
+//        Node<Integer> n33 = new Node<Integer>(4);
+//        n22.setLeft(n31);
+//        n22.setRight(n32);
+//        n23.setLeft(n33);
+//        n11.setLeft(n21);
+//        n11.setRight(n22);
+//        n12.setRight(n23);
+//        nodes.setLeft(n11);
+//        nodes.setRight(n12);
+        
+		boolean end = false;
+		while (!end) {
+			end = processMenu(nodes);
+			BinaryTreePrinter.printNode(nodes);
+		}
+	}
+
+	private static boolean processMenu(Node<Integer> nodes) {
+		System.out.println("1) Prefix");
+		System.out.println("2) Hauteur");
+		System.out.println("3) Taille");
+		System.out.println("4) Equilibré");
+		System.out.println("5) Rotation gauche");
+		System.out.println("6) Quitter");
+		sc.nextLine();
+		switch (sc.nextInt()) {
+		case 1:
+			nodes.prefix();
+			break;
+		case 2:
+			System.out.println(nodes.height());
+			break;
+		case 3:
+			System.out.println(nodes.size());
+			break;
+		case 4:
+			System.out.println(nodes.isBalanced());
+			break;
+		case 5:
+			nodes = nodes.leftRotation();
+			break;
+		default:
+			return true;
+		}
+		return false;
+	}
+
+	
+	
+	public static Node<Integer> initialize() {
+		System.out.println("Entez la valeur du noeud");
+		Node<Integer> nodes = new Node<Integer>(sc.nextInt());
+		System.out.println("Noeud gauche ? Y/N\n");
+		if (sc.next().equals("Y")) {
+			nodes.setLeft(initialize());
+		}
+		System.out.println("Noeud droit ? Y/N");
+		if (sc.next().equals("Y")) {
+			nodes.setRight(initialize());
+		}
+		return nodes;
+	}
+}
---
+++ src/arbre/Node.java
@@ -0,0 +1,98 @@
+package arbre;
+
+class Node<T extends Comparable<?>> implements Cloneable {
+    private Node<T> left, right;
+    private T value;
+
+    public Node(T value) {
+        this.value = value;
+    }
+    
+    public Node<T> clone() {
+    	Node<T> clonedNode = new Node<T>(this.getValue());
+    	clonedNode.setLeft(this.getLeft());
+    	clonedNode.setRight(this.getRight());
+    	return clonedNode;
+    }
+    
+    public Integer size() {
+		if (isEmpty())
+			return 0;
+		Integer leftSize = (this.getLeft() != null) ? this.getLeft().size() : 0;
+		Integer rightSize = (this.getRight() != null) ? this.getRight().size() : 0;
+		return 1 + leftSize + rightSize;
+	}
+
+    public Integer height() {
+		if (isEmpty())
+			return -1;
+		if (this.getLeft() == null && this.getRight() == null)
+			return 0;
+		Integer leftHeight = (this.getLeft() != null) ? this.getLeft().height() : 0;
+		Integer rightHeight = (this.getRight() != null) ? this.getRight().height() : 0;
+		return 1 + Math.max(leftHeight, rightHeight);
+	}
+
+	public void prefix() {
+		if (isEmpty())
+			return;
+		System.out.println(this.getValue());
+		
+		if (this.getRight() != null)
+			this.getRight().prefix();
+		
+		if (this.getLeft() != null)
+			this.getLeft().prefix();
+	}
+
+	public boolean isBalanced() {
+		if (isEmpty())
+			return false;
+		Integer leftHeight = (this.getLeft() != null) ? this.getLeft().height() : -1;
+		Integer rightHeight = (this.getRight() != null) ? this.getRight().height() : -1; 
+		Integer leftHeightMinusRightHeight = leftHeight - rightHeight;
+		boolean isLeftBalanced = (this.getLeft() != null) ? this.getLeft().isBalanced() : true;
+		boolean isRightBalanced = (this.getRight() != null) ? this.getRight().isBalanced() : true;
+		return Math.abs(leftHeightMinusRightHeight) <= 1 && isLeftBalanced && isRightBalanced;
+	}
+
+	public boolean isEmpty() {
+		return this.getValue() == null;
+	}
+
+	public Node<T> leftRotation() {
+		if (isEmpty() || this.getRight() == null) 
+			return null;
+		Node<T> newNodes = this.clone().getRight();
+		if (newNodes == null)
+			return null;
+		if (newNodes.getLeft() != null) 
+			this.setRight(newNodes.getLeft());
+		newNodes.setLeft(this);
+		return newNodes;
+	}
+
+	public Node<T> getLeft() {
+		return left;
+	}
+
+	public void setLeft(Node<T> left) {
+		this.left = left;
+	}
+
+	public Node<T> getRight() {
+		return right;
+	}
+
+	public void setRight(Node<T> right) {
+		this.right = right;
+	}
+
+	public T getValue() {
+		return value;
+	}
+
+	public void setValue(T value) {
+		this.value = value;
+	}
+}
